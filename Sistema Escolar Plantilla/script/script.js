384aa020b2d515f987304712cba7e6a5c1b521b4
Roberto Mora <robertomorado.coro2@gmail.com>
2023-05-02 13:15:09 -0400

    sistema plantilla

---
+++ Sistema Escolar Plantilla/script/script.js
@@ -0,0 +1,7 @@
+const btnEnviar = document.getElementById("#enviar");
+const modal = document.querySelector(".modal_main");
+
+
+btnEnviar.addEventListener("click", ()=>{
+    modal.style.display = "flex"
+})
